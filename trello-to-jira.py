4ab8b16fb80126c7b19c0161375b7378d87b30d1
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2020-06-19 15:53:55 -0500

    - Added the ability to import checklist as subtasks and comments history

--- trello-to-jira.py
+++ trello-to-jira.py
@@ -24,7 +24,8 @@ failed_conversion   = 0
 # Lot of Hardcoding for now
 # The Goal is to get all the cards from Trello Board A and import then into JIRA Project J
 
-src_trello_board = "Foundations Backlog"
+# src_trello_board = "Foundations Backlog"
+src_trello_board = "THE Board"
 dest_jira_project= "FS"
 
 
@@ -46,6 +47,7 @@ labels_to_versions = [  "20.04",
                         "20.04.1",
                         "20.04.2",
                         "20.10",
+                        "21.04"
                         ]
 
 # What Label need to be cleaned up before importing
@@ -54,8 +56,10 @@ labels_cleanup = [  "General Distro",
                     "netplan",
                     "Action Item",
                     "Infrastructure",
-                    "Roadmap Item",
-                    "Epic"]
+                    "Epic",
+                    "raspi",
+                    "Alpha Squad",
+                    "Beta Squad"]
 
 #Initialize Trello API Token
 trello = trello_api()
@@ -73,7 +77,17 @@ def remove_and_add(search_str,in_list,target_list,new_str=""):
             target_list.append(new_str)
         else:
             target_list.append(search_str)
-
+ 
+def handle_checklist(card):
+    clists = card.checklists
+    # There could be multiple checklist or no checklist
+    if clists:
+        for clist in clists:
+            # print(clist.name)
+            for item in clist.items:
+                # Looks like this item is not done yet
+                if not item['checked']:
+                    print(" * {}".format(item['name']))
 
 def convert_to_jira(card):
     global skipped_cards, converterd_cards, failed_conversion
@@ -120,7 +134,8 @@ def convert_to_jira(card):
 
     card_labels = [x.replace(' ','') for x in card_labels]
 
-    print("Card Summary : {}".format(card.name))
+    # TODO implement verbose mode and print the text below
+    # print("Card Summary : {}".format(card.name))
     # print("Description : {}".format(card.description))
     # print("Card Type : {}".format(jira_item_type))
     # print("Assignee : ")
@@ -133,6 +148,7 @@ def convert_to_jira(card):
 
     # JIRA Magic Here 
     # Test if the card has already been imported in the past
+    # TODO Search could fail
     already_imported = jira_client.search_issues(
         "project = \"{}\" AND trello_card = \"{}\"".format(dest_jira_project,jira_item_id))
 
@@ -162,20 +178,57 @@ def convert_to_jira(card):
         #importing labels
         issue_dict["labels"] = card_labels
 
-        try:
-            new_issue = jira_client.create_issue(fields=issue_dict)
+        # try:
+        new_issue = jira_client.create_issue(fields=issue_dict)
 
-            #importing attachments
-            if card.attachments:
-                for url in card.attachments:
-                    link = {'url':url['url'],'title':url['name']}
-                    jira_client.add_simple_link(new_issue,object=link)
-            
-            print("Created {}/browse/{}".format(jira.server,new_issue.key))
-            converterd_cards += 1
-        except:
-            print("Error converting card: {}".format(jira_item_id))
-            failed_conversion += 1
+        # importing attachments
+        attachments = card.attachments 
+        if attachments:
+            for url in attachments:
+                link = {'url':url['url'],'title':url['name']}
+                jira_client.add_simple_link(new_issue,object=link)
+
+        # importing comments            
+        comments = card.comments
+        # There could be multiple comments or no comments
+        if comments:
+            for comment in comments:
+                jira_client.add_comment(new_issue,"{} on {} :\n{}".format(
+                    comment['memberCreator']['fullName'],
+                    comment['date'][:10],
+                    comment['data']['text']))
+        
+        # Creating subtasks if needed
+        clists = card.checklists
+        # There could be multiple checklist or no checklist
+        if clists:
+            # Only deal with with first checlist (majority of our cards)
+            children=[]
+            for item in clists[0].items:
+                # Looks like this item is not done yet
+                if not item['checked']:
+                    subissue_dict = {
+                        'project': dest_jira_project,
+                        'summary': item['name'],
+                        'issuetype': {'name':'Sub-task'},
+                        'parent' : {'key':new_issue.key},
+                        'customfield_10031' : jira_item_id,
+                        'components' : jira_item_components,
+                        'fixVersions' : jira_item_versions,
+                        'labels' : card_labels
+                    }
+                    children.append(subissue_dict)
+
+            if children:
+                jira_client.create_issues(field_list=children)
+
+
+
+        print("Created {}/browse/{}".format(jira.server,new_issue.key))
+        converterd_cards += 1
+        # except:
+        #     print("Error converting card: {}".format(jira_item_id))
+        #     failed_conversion += 1
 
     print("")
 
@@ -203,8 +256,10 @@ for card in the_board.open_cards():
 
 print ("Found {} Cards to convert to {} JIRA Project".format(len(the_board_cards),dest_jira_project))
 
-for card in the_board_cards:
-    convert_to_jira(card)
+# for card in the_board_cards:
+#     convert_to_jira(card)
+
+convert_to_jira(the_board_cards[6])
 
 print("Convertion Report:")
 print("\t{} Cards Skipped".format(skipped_cards))
